b7d8ab2fbe1c49c77391ecbf9f4c6280c5c552f7
HoYeon-068 <46436420+HoYeon-068@users.noreply.github.com>
2025-09-05 22:52:21 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.05 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] num_list) {
+        
+        int x=1;
+        int y=0;
+        for(int i=0;i<num_list.length;i++){
+            x*=num_list[i];
+            y+=num_list[i];
+        }
+        y*=y;
+        
+        return x<y?1:0;
+    }
+}
