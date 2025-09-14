2de57ca96e7c84e7484a69fb617aecc39335f684
HoYeon-068 <46436420+HoYeon-068@users.noreply.github.com>
2025-09-14 15:38:46 +0900

    [Silver IV] Title: 주몽, Time: 176 ms, Memory: 16520 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int n=Integer.parseInt(br.readLine());
+		int m=Integer.parseInt(br.readLine());
+		
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int arr[]=new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		int start=0,end=n-1;
+		int sum=0,count=0;
+		while (start<end) {
+			sum=arr[start]+arr[end];
+			if (sum==m) {
+				count++;
+				start++;
+				end--;
+			}else if(sum>m) {
+				end--;
+			}else {
+				start++;
+			}
+		}
+		System.out.println(count);
+	}//main
+
+}//class
