2e237d063e073155d3595a4b007ff547ef8e8ef4
HoYeon-068 <46436420+HoYeon-068@users.noreply.github.com>
2025-09-21 21:40:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 285.16 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+     public static String solution(String[] participant, String[] completion) {
+	        
+		 	
+		 	Arrays.sort(participant);
+		 	Arrays.sort(completion);
+		 	
+		 	String answer=participant[participant.length-1];
+		 	
+		 	
+		 	for (int i = 0; i < completion.length; i++) {
+		 		if (!participant[i].equals(completion[i])) {
+					answer=participant[i];
+					break;
+				}
+			}
+		 	System.out.println(answer);
+		 	
+	        return answer;
+	    }
+}
