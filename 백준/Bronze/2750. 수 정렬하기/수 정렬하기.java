d5f4f92d5ffef0506adc341493d31fe7dbb3a679
HoYeon-068 <46436420+HoYeon-068@users.noreply.github.com>
2025-10-05 21:52:00 +0900

    [Bronze II] Title: 수 정렬하기, Time: 148 ms, Memory: 16648 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,23 +1,43 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int[] arr=new int[n];
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		for (int i = 0; i < arr.length; i++) {
-			arr[i]=sc.nextInt();
+		int N=Integer.parseInt(br.readLine());
+		
+		int arr[]=new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i]=Integer.parseInt(br.readLine());
 		}
 		
-		Arrays.sort(arr);
+		int temp;
 		
-		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
+		for (int i = 0; i < N; i++) {
+			for (int j = 1; j < N-i; j++) {
+				if (arr[j-1]>arr[j]) {
+					
+					temp=arr[j-1];
+					arr[j-1]=arr[j];
+					arr[j]=temp;
+					
+				} 
+			}
 		}
 		
+		
+		
+		for (int i = 0; i < N; i++) {
+			bw.write(arr[i]+"\n");
+		}
+		bw.flush();
 	}
 
 }
