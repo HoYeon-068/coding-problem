8f444ff0f2a3b56696b23928aa06e4cbf8445b58
HoYeon-068 <46436420+HoYeon-068@users.noreply.github.com>
2025-11-22 19:52:06 +0900

    [level 0] Title: A 강조하기, Time: 0.02 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,7 @@
+class Solution {
+   public static String solution(String myString) {
+        String lower_string=myString.toLowerCase();
+        
+        return lower_string.replace('a', 'A');
+    }
+}
