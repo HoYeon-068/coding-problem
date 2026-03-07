60c32454bd54f5cebc8d766ccfa4d171fcc45d38
HoYeon-068 <46436420+HoYeon-068@users.noreply.github.com>
2026-03-07 21:00:20 +0900

    [Bronze I] Title: N번째 큰 수, Time: 344 ms, Memory: 29404 KB -BaekjoonHub

---
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+	    int n = sc.nextInt();
+        
+        int arr[]=new int[10];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<10;j++){
+                arr[j]=sc.nextInt();
+            }
+            Arrays.sort(arr);
+            System.out.println(arr[7]);
+        }
+        
+        sc.close();
+    }
+}
