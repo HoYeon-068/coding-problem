b0e016fc7b39fb39f2092ef6b7493ecc324a72a2
HoYeon-068 <46436420+HoYeon-068@users.noreply.github.com>
2026-04-02 21:12:31 +0900

    [Bronze V] Title: Plane, Time: 184 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {    
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+
+        int result=0;
+        
+        
+        for(int i=0;i<2;i++){
+            int arr[]=new int[2];
+            for(int j=0;j<2;j++){
+                    arr[j]=sc.nextInt();
+            }
+            result+=arr[0]*arr[1];
+        }
+        
+        System.out.print(result);
+        
+        
+        sc.close();
+    }
+}
